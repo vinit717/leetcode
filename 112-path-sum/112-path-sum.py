34297818d603f86229aed5442f65bdf2c8bba365
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-04 17:40:23 +0530

    Time: 81 ms (42.11%), Space: 15 MB (56.49%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -6,14 +6,12 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if root is None: 
+        if not root:
             return False
+
+        if not root.left and not root.right and root.val == targetSum:
+            return True
         
-        # [2] termination condition
-        if root.left is None and root.right is None:
-            return targetSum == root.val
-        
-        # [3] recursive exploration of the tree
         targetSum -= root.val
+
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
-        
