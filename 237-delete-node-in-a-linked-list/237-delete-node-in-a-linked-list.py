b62221287b48c830a7f7a9dff678743b7a237330
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-08 11:20:11 +0530

    Time: 84 ms (19.20%), Space: 14.3 MB (14.83%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.py
@@ -10,7 +10,10 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        temp = node.next # making a temp pointer to save the next node.
+        '''temp = node.next # making a temp pointer to save the next node.
         node.val = temp.val # copying the value of next node value to the current node.
         node.next = temp.next # update the reference to the next to next node.
+        '''
+        node.val = node.next.val # copying next node value in the currernt node.
+        node.next = node.next.next # to delete the next node that we copied, we are updating the reference of the node to next to next node.
         
