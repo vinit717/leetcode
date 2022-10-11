0cc9a435fa12bc080d01c6b3293b27f571ce12fa
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-11 11:30:59 +0530

    Time: 70 ms (82.20%), Space: 15.2 MB (85.80%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -1,7 +1,18 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        for i in range(len(num) - 1, -1, -1) :
-            if num[i] in {'1','3','5','7','9'} :
-                return num[:i+1]
-        return ''
+        # We have to just find the last index of an odd number, then slice the number upto that index,  becuase an odd number always ends with a number which is not divisible by 2 :)
         
+        # Lets take the last index of an odd number as -1
+        last_ind = -1
+        
+        # Iterate through all the numbers for finding a odd number that appears on the last.
+        for i , j in enumerate(num[::-1]):
+            if(int(j) % 2 != 0):
+                last_ind = len(num) - i
+                break
+        
+        # If there is no odd number, return empty string.
+        if(last_ind == -1): return ""
+                
+        # Or return the string upto that index.
+        return(num[:last_ind])
