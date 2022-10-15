151903da1d8e3ac4d42320ffe97c45844019c34b
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-15 20:57:05 +0530

    Time: 89 ms (72.02%), Space: 13.7 MB (70.77%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        a=[]
+        for i in range(len(nums)):
+            a.append(nums[i])
+        for i in range(len(nums)):
+            a.append(nums[i])
+        return a
+        
+        
