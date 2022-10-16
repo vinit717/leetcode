037e1e02848f8c13f6533b9edd1bd1039ec1a645
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-16 11:13:43 +0530

    Time: 106 ms (13.36%), Space: 13.5 MB (87.84%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -5,14 +5,10 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        l=0
-        r=len(nums)//2
         ans=[]
-        while l<len(nums)//2 and r<len(nums):
-            ans.append(nums[l])
-            l+=1
-            ans.append(nums[r])
-            r+=1
+        for i in range (n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
         return ans
        
             
