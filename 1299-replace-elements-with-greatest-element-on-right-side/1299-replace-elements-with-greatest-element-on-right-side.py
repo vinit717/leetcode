9b14e2b3161205809e3564e371422fe51fbc5d61
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-19 11:30:45 +0530

    Time: 225 ms (56.68%), Space: 14.9 MB (50.81%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -3,9 +3,15 @@ class Solution(object):
         """
         :type arr: List[int]
         :rtype: List[int]
+        
         """
-        mx=-1
-        for i in range(len(arr)-1,-1,-1):
-            arr[i],mx=mx,max(mx,arr[i])
+        curr_max = arr[-1]
+        arr[-1] = -1
+        for i in range(len(arr)-2,-1,-1):
+            curr = arr[i]
+            arr[i] = curr_max
+            if curr>curr_max:
+                curr_max = curr
         return arr
+    
         
