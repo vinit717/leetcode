483115cb99f34d10cd6c008457a4fabcd7b95cf7
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-11-24 12:57:33 +0530

    Time: 79 ms (5.14%), Space: 13.8 MB (72.26%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+           # TC = O(n) ; SC = O(n)
+        # because we need to iterate over the string once
+        # and we are using a check dictionary and stack for space 
+        # so space complexity = O(n)
+        
+        n = len(s)
+        if n&1 or n < 2:
+            return False
+        
+        # odd length strings wont ever be balanced/valid
+        # similarly strings with a single bracket wont be valid
+        
+        check = {'(':')', '{':'}','[':']'}
+        stack1 = []
+        for bracket in s:
+            # if ith character is opening bracket
+            # then add it to stack 
+            if bracket in check:
+                stack1.append(bracket)
+            # otherwise if the stack is empty 
+            # or if the closing bracket is not of the same type as the opening one
+            # then sequence is not valid
+            elif len(stack1) == 0 or check[stack1.pop()] != bracket:
+                return False
+            
+        # if length of the stack is 0 meaning that all opening and closing 
+        # brackets matched then ans is Valid
+        return len(stack1) == 0
+        
